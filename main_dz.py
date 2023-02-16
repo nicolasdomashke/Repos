ece06d1f9eb0c6468b1cddcdb75fcda13da606c4
NickDom <NicolasDomashke@gmail.com>
2023-02-16 13:26:53 +0300

    Find solutions for square equation

--- main_dz.py
+++ main_dz.py
@@ -6,11 +6,11 @@ d = b * b - 4 * a * c
 if d < 0:
     print("no solutions")
 elif d == 0:
-    x = -b / 2
+    x = -b / (2 * a)
     print("found one solution:")
     print(x)
 else:
-    x1 = (-b + sqrt(d)) / 2
-    x2 = (-b - sqrt(d)) / 2
+    x1 = (-b + sqrt(d)) / (2 * a)
+    x2 = (-b - sqrt(d)) / (2 * a)
     print("found two solutions:")
     print(x1, x2)
